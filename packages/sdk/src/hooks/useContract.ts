e9de7c549d8d29ab44d9b82154cdfd54d163e6ed
Ian Hunter <ian@users.noreply.github.com>
2022-09-05 10:25:03 -0700

    fix useContract to be abi null safe

--- packages/sdk/src/hooks/useContract.ts
+++ packages/sdk/src/hooks/useContract.ts
@@ -1,16 +1,22 @@
-import { Contract, providers, Signer } from "ethers"
-import { useContract as useWagmiContract, useSigner } from "wagmi"
+import { Contract, ethers, providers, Signer } from "ethers"
+import { useEffect, useState } from "react"
+import { useSigner } from "wagmi"
 
 import { useDrop } from "./useDrop"
 
-export function useContract(provider?: Signer | providers.Provider): Contract {
+export function useContract(
+  provider?: Signer | providers.Provider
+): Contract | null {
   const { address, abi } = useDrop()
   const { data: signer } = useSigner()
-  const contract = useWagmiContract({
-    addressOrName: address,
-    contractInterface: abi,
-    signerOrProvider: provider || signer
-  })
+  const [contract, setContract] = useState<ethers.Contract>()
+  useEffect(() => {
+    if (!address || !abi) {
+      setContract(null)
+    } else {
+      setContract(new ethers.Contract(address, abi, provider || signer))
+    }
+  }, [])
 
   return contract
 }
